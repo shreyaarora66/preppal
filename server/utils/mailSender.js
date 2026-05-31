1197d98e550598784e9313ba648e3f9113bcb294
jay-singla <jaysingla457@gmail.com>
2026-05-31 14:21:02 +0530

    Switch to Brevo SMTP

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,8 +4,8 @@ const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
